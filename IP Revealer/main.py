0058b3c5b5b5e7ef403e84713b06aac62a9a8206
ndonkoHenri <robotcoder4@protonmail.com>
2023-05-19 01:50:39 +0200

    replace ipify requests with `page.client_id`

--- IP Revealer/main.py
+++ IP Revealer/main.py
@@ -1,80 +1,97 @@
-import time
 import flet as ft
-import requests
 
 
 def main(page: ft.Page):
     page.title = "IP Revealer"
-    page.window_center()
-    page.window_always_on_top = True
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.vertical_alignment = page.horizontal_alignment = "center"
-    page.window_min_width, page.window_min_height = 312, 124
-    page.window_width, page.window_height = 386, 201
+    page.fonts = {
+        "PJS": "/fonts/Plus_Jakarta_Sans/PlusJakartaSans-VariableFont_wght.ttf",
+        "Stick": "/fonts/Stick/Stick-Regular.ttf"
+    }
+    page.theme = ft.Theme(font_family="PJS")
 
-    ip_text = ft.Ref[ft.Text]()
+    # in desktop mode
+    if not page.web:
+        page.window_center()
+        # page.window_always_on_top = True
+        page.window_min_width, page.window_min_height = 312, 124
+        page.window_width, page.window_height = 386, 201
 
-    def fetch_ip():
+    def reveal_ip(e):
         """
-        Query the ipify service (https://www.ipify.org) to retrieve this device's public IP address.
-
-        :rtype: string
-        :returns: The public IP address of the requesting device as a string.
-        :raises: RequestError if the web request failed
+        The reveal_ip function is called when the user clicks on the eye icon.
+        It hides both the eye and blur containers, which makes it so that only the IP address is visible.
         """
-        error = ""
-        try:
-            resp = requests.get('https://api.ipify.org')
-        except requests.RequestException:
-            error = "The request failed, most likely due to an networking error. Check you Internet connection."
-        else:
-            if resp.status_code != 200:
-                error = 'Received an invalid status code from ipify:' + str(
-                    resp.status_code) + '. The service might be experiencing issues.'
-            else:
-                ip_text.current.value = resp.text
-                page.update()
-
-        if error:
-            for i in [error, "Retrying in some few seconds..."]:
-                print(i)
-                page.show_snack_bar(
-                    ft.SnackBar(
-                        content=ft.Text(i),
-                        open=True
-                    )
-                )
-                time.sleep(9)
-
-            # retry again
-            fetch_ip()
+        eye_container.visible = False
+        blur_container.visible = False
+        page.update()
 
     page.add(
         ft.Container(
             content=ft.Column(
                 controls=[
-                    ft.Text("</Your Public IP>", weight=ft.FontWeight.BOLD, size=15),
-                    ft.Text("000.000.000", ref=ip_text, weight=ft.FontWeight.BOLD, size=25, color=ft.colors.WHITE)
+                    ft.Text("</Your Public IP>", weight=ft.FontWeight.BOLD, size=25, selectable=True),
+                    ft.Stack(
+                        [
+                            ft.Container(
+                                ft.Text(
+                                    page.client_ip if page.client_ip else "No IP found :(",
+                                    weight=ft.FontWeight.BOLD,
+                                    font_family="Stick",
+                                    size=40,
+                                    selectable=True
+                                ),
+                                alignment=ft.alignment.center
+                            ),
+                            blur_container := ft.Container(
+                                width=350,
+                                height=55,
+                                blur=ft.Blur(15, 15),
+                            ),
+                            eye_container := ft.Container(
+                                content=ft.IconButton(
+                                    ft.icons.REMOVE_RED_EYE,
+                                    on_click=reveal_ip
+                                ),
+                                alignment=ft.alignment.bottom_center,
+                                padding=ft.Padding(0, 5, 0, 0)
+                            ),
+                        ]
+                    ),
+                    ft.Container(
+                        content=ft.Text(
+                            "Made with ❤ by the ",
+                            spans=[
+                                ft.TextSpan(
+                                    "@TheEthicalBoy",
+                                    ft.TextStyle(
+                                        color=ft.colors.BLUE,
+                                        decoration=ft.TextDecoration.UNDERLINE,
+                                        decoration_color=ft.colors.BLUE,
+                                        decoration_thickness=0.5,
+                                    ),
+                                    url="https://github.com/ndonkoHenri",
+                                )
+                            ],
+                            selectable=True,
+                            size=10
+                        )
+                    )
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                 expand=True
             ),
-            # I came up with this gradient using Flutils - https://flutils.pages.dev
-            gradient=ft.LinearGradient(
-                colors=['white38', 'white12'],
-                tile_mode=ft.GradientTileMode.CLAMP,
-                rotation=4.712,
-                stops=[0.01, 0.6],
-                begin=ft.Alignment(x=-1, y=0),
-                end=ft.Alignment(x=1, y=0),
-                type='linear'
-            ),
-            width=270
+            width=500,
         )
     )
 
-    # go get the IP
-    fetch_ip()
 
-
-ft.app(main)
+ft.app(
+    main,
+    view=ft.WEB_BROWSER,
+    web_renderer="html",
+    assets_dir="assets",
+    # use_color_emoji=True
+)
